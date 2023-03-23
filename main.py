10c958dfe6f72fad9aa5eef6ab40091bf6b88f0b
voltagedomo <98976613+voltagedomo@users.noreply.github.com>
2023-03-23 14:08:31 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Simple Calc in Python
+
+# Input
+num1 = float(input("Please enter a number: "))
+
+num2 = float(input("Pleae enter another number: "))
+
+# Process
+
+total = num1 + num2
+
+# Output
+print("The sum of the two numbers is " + str(total) + ".")
